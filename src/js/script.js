c5141775e6f94f97887d7c4d1c8e6a2de461265e
Kornienko  Dmytro <caxapz1q@gmail.com>
2025-08-27 11:06:33 +0300

    Completed activities section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
-import "purecss/build/grids-min.css";
-import "purecss/build/grids-responsive-min.css";
+// import "purecss/build/grids-min.css";
+// import "purecss/build/grids-responsive-min.css"
+
+
 import "/src/sass/style.scss";
