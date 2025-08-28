dc7c9fef2842e2bd7c0f064e79c30ca38efcc423
Kornienko  Dmytro <caxapz1q@gmail.com>
2025-08-28 16:01:00 +0300

    add works section and start slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
 // import "purecss/build/grids-min.css";
 // import "purecss/build/grids-responsive-min.css"
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
-
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+
+        1920: {
+            slidesPerView: 3,
+            spaceBetween: 35,
+        },
+    }
+});
+
