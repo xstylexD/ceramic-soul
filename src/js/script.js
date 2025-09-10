12eda14cc2fb28eb1c85129f2e21472705a90526
Kornienko  Dmytro <caxapz1q@gmail.com>
2025-09-10 11:27:23 +0300

    Completed main page

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,20 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
+
 const swiper = new Swiper('.swiper', {
     modules: [Navigation, Pagination],
     slidesPerView: 1,
