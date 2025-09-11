7a861e94e03ab9bf36846f565a74eb8a8b45cdac
Kornienko  Dmytro <caxapz1q@gmail.com>
2025-09-11 16:44:16 +0300

    start new page Blog

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
 			input: {
 				main: resolve(__dirname, "index.html"),
 				catalog: resolve(__dirname, "catalog.html"),
+				blog: resolve(__dirname, "blog.html"),
 			},
 		},
 	},
